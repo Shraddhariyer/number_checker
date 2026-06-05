2a18e85654ae629c9790af48f4bbee8acdb1493f
Shraddhariyer <130347830+Shraddhariyer@users.noreply.github.com>
2026-06-05 23:48:59 +0530

    added wrong testcase

--- test_num_checker.py
+++ test_num_checker.py
@@ -2,6 +2,6 @@ from num_checker import is_positive
 def test_positive():
     assert is_positive(5)==True
 def test_negative():
-    assert is_positive(-5)==False
+    assert is_positive(-5)==True
 def test_zero():
     assert is_positive(0)==False
